24184f9d2cd335272938ccfa2f01d63bbc74f750
RiyaRai01 <riyarai.safali@gmail.com>
2025-12-31 01:24:33 +0530

    Day2 task completed.

--- Day-2/api_data_fetcher.py
+++ Day-2/api_data_fetcher.py
@@ -1,21 +1,46 @@
 import requests
+import json
 
-def fetch_api_data():
-    api_url = "https://api.restful-api.dev/objects" 
-    response = requests.get(url=api_url)
+url = "https://api.github.com/repos/kubernetes/kubernetes"
+
+def fetch_repo_data(url):
+    response = requests.get(url)
+    return response.json()
+
+def process_data(data):
+        usefull_data = {
+        "id" : data["id"],
+        "node_id" : data["node_id"],
+        "full name" : data["full_name"],
+        "subscribers_count" : data["subscribers_count"],
+        }
+        return usefull_data
     
-    datalist = response.json()
+def save_to_file(data, filename="output.json"): 
+    with open(filename, "w") as output_file:
+        json.dump(data,output_file, indent=4) 
+    print(f"Data saved to {filename}")  
     
-    for item in datalist:
-        for key, value in item.items():
-            if key == "name":
-                continue
-            else:
-                print(key,value)
+def main():
+    print("Fetching data from GitHub API...")
+    repo_data = fetch_repo_data(url)
+    print(repo_data)
     
-fetch_api_data()
+    print("\nProcessing data...")
+    processed_data = process_data(repo_data)
+    print(processed_data)
+    
+    print("\nSaving data to file...")
+    save_to_file(processed_data)    
+    
+if __name__ == "__main__":
+    main()
+    
 
 
+
+
+    
         
     
 
